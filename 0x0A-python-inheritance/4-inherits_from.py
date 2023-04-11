2d1c4c85850c20a768f8a94e7c4d1840003728d0
ToqaZidan <toqazidan@hotmail.com>
2023-04-11 16:26:53 -0700

    task4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,8 @@
 
 def inherits_from(obj, a_class):
     """
-    Checks if the given object is an instance of a class that inherited (directly or indirectly) from the specified class.
+    Checks if the given object is an instance of a class that
+    inherited (directly or indirectly) from the specified class.
 
     :param obj: The object to be  checked.
     :param a_class: The class to check against.
@@ -12,4 +13,14 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from a_class,
         otherwise False.
     """
-    return isinstance(obj, type) and issubclass(obj, a_class)
+
+        base_classes = obj.__class__.__bases__
+
+    if a_class in base_classes:
+        return True
+
+    for base_class in base_classes:
+        if inherits_from(base_class, a_class):
+            return True
+
+    return False
