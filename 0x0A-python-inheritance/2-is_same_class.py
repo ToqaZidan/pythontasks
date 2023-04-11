f0ba8fc3a09ef280ac4e24e43e908c2dd6038ad3
ToqaZidan <toqazidan@hotmail.com>
2023-04-11 15:08:05 -0700

    task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Define  class checking a function that object is exactly
+an instance of the specified class
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+Define a function that object is exactly
+an instance of the specified class
+
+Args:
+obj: An object to be checked
+a_class: class which object will be checked in
+
+Return: True - if object is instance of class 
+    False - otherwise
+"""
+
+
+return obj.__class__ is a_class
+
