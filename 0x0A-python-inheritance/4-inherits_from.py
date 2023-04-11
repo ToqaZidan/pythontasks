534d9aaa86905bcfac87a757d5e80ec26fdc0212
ToqaZidan <toqazidan@hotmail.com>
2023-04-11 16:12:44 -0700

    task4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a checking inheritance function"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if the given object is an instance of a class that inherited (directly or indirectly) from the specified class.
+
+    :param obj: The object to be  checked.
+    :param a_class: The class to check against.
+    :return: True if obj is an instance of a class that inherited
+    (directly or indirectly) from a_class,
+        otherwise False.
+    """
+    return isinstance(obj, type) and issubclass(obj, a_class)
