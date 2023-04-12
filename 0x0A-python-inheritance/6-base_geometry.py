58df20576cc64ccbf3a88d24b38bff60b53b1136
ToqaZidan <toqazidan@hotmail.com>
2023-04-11 17:28:29 -0700

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define an Empty Base class"""
+
+
+class BaseGeometry:
+    "Class BaseGeometry which is empty base class"
+
+    def area(self):
+        """Not implemented"""
+
+        raise Exception("area() is not implemented")
