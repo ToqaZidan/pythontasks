df8585604c031413fc170684dcc8fe30d0eee64b
ToqaZidan <toqazidan@hotmail.com>
2023-04-13 13:32:08 -0700

    task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a function that reads a file"""
+
+
+def read_file(filename=""):
+    """"
+    Function that reads a file, file permission or file doesn't exist
+    are not maneged, and print a text file to stdout
+    """
+
+    with open (filename, "r", encoding="UTF-8") as new_file:
+        print (new_file.read(), end="")
