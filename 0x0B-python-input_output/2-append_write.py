4ace4112dcb720d9a97b66f5be1197051393e1e0
ToqaZidan <toqazidan@hotmail.com>
2023-04-13 13:57:03 -0700

    task2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Define a function that append a string to a text file
+"""
+
+
+
+def write_file(filename="", text=""):
+    """
+    Function that append without truncate textfile if it was found, creat it if it
+    was not found.
+
+    Args:
+    filename: file to be written over.
+    text: text to be written.
+
+    Return:
+    number of characters written.
+    """
+    with open(filename, "a", encoding="UTF-8") as new_file:
+        return new_file.append(text)
