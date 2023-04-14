4094bf47e42e41b07ed1b78fb543dd6a42f3e4f8
ToqaZidan <toqazidan@hotmail.com>
2023-04-13 19:10:59 -0700

    task

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""Define a class MyInt which inherites fron Int """
+
+
+class MyInt(int):
+    """A subclass of `int` that flips the behavior of the `==` and `!=` operators.
+    
+    Inherits from:
+        int: The built-in integer class
+    
+    Example:
+        x = MyInt(1)
+        y = MyInt(2)
+        print(x == y)  # True
+        print(x != y)  # False
+    """
+    
+    def __eq__(self, other):
+        """Inverts the behavior of the `==` operator."""
+        return super().__ne__(other)
+    
+    def __ne__(self, other):
+        """Inverts the behavior of the `!=` operator."""
+        return super().__eq__(other)
