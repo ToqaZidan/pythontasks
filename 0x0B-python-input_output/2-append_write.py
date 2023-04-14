414b5bf4dc1341c407f1b0f71f22a3dedeafda26
ToqaZidan <toqazidan@hotmail.com>
2023-04-13 19:01:12 -0700

    tasks

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,11 +10,11 @@ def write_file(filename="", text=""):
     if it was found, creat it if it was not found.
 
     Args:
-    filename: file to be written over.
-    text: text to be written.
+        filename: file to be written over.
+        text: text to be written.
 
     Return:
-    number of characters written.
+        number of characters written.
     """
     with open(filename, "a", encoding="UTF-8") as new_file:
         return new_file.write(text)
